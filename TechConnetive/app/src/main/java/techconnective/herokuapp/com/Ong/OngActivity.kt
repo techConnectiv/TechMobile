e5e4de1b7ee49c773415b7432f9f11d7fe9e70a5
Danilo Jesus <danilo25oliveira@gmail.com>
2020-06-27 05:54:00 -0300

    correção do bug na tela das ongs

--- TechConnetive/app/src/main/java/techconnective/herokuapp/com/Ong/OngActivity.kt
+++ TechConnetive/app/src/main/java/techconnective/herokuapp/com/Ong/OngActivity.kt
@@ -1,15 +1,18 @@
 package techconnective.herokuapp.com.Ong
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.graphics.drawable.toDrawable
+import connection.objetos.Usuario
 import kotlinx.android.synthetic.main.activity_event_detail.*
 import kotlinx.android.synthetic.main.activity_ong.*
 import techconnective.herokuapp.com.R
+import techconnective.herokuapp.com.doacao.FormDonateActivity
 
 class OngActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +35,16 @@ class OngActivity : AppCompatActivity() {
         favorite?.setOnClickListener {
             favorite.setImageResource(R.drawable.ic_favorited)
 
-            Toast.makeText(this, getString(R.string.favorited_messenge), Toast.LENGTH_LONG).show()
+            Toast.makeText(this, getString(R.string.favoritar), Toast.LENGTH_LONG).show()
+        }
+
+        btn_doacao?.setOnClickListener{
+            val intent = Intent(this, FormDonateActivity::class.java)
+
+            intent.putExtra("nameOng", nameOng)
+            intent.putExtra("icon", iconOng)
+
+            startActivity(intent)
         }
     }
 }
